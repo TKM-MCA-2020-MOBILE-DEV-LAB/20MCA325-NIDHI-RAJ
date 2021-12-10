2363d7a8573c8b2538ed5403c179a9fe10ab77e4
NidhiRj <68844226+NidhiRj@users.noreply.github.com>
2021-12-10 19:59:19 +0530

    Intent Creation

---
+++ CO1/PG5/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.intentcreation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void callSecondActivity(View view){
+        Intent i = new Intent(getApplicationContext(), SecondActivity.class);
+        startActivity(i);
+    }
+}
