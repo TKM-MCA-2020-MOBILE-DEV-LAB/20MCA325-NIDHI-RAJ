161a955de061aa8354982139636194d4212f595f
NidhiRj <68844226+NidhiRj@users.noreply.github.com>
2022-03-03 14:12:10 +0530

    CO4 Q3 Q5

---
+++ CO4/Q5/contactsFragment.java
@@ -0,0 +1,46 @@
+package com.example.navigation.ui.contacts;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.navigation.R;
+import com.example.navigation.databinding.FragmentContactsBinding;
+import com.example.navigation.databinding.FragmentGalleryBinding;
+import com.example.navigation.ui.gallery.GalleryViewModel;
+
+public class contactsFragment extends Fragment {
+    private ContactsViewModel contactsViewModel;
+    private @NonNull FragmentContactsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        contactsViewModel =
+                new ViewModelProvider(this).get(ContactsViewModel.class);
+
+        binding = FragmentContactsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textContacts;
+        contactsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+
+
+    }
+
+}
